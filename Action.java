c1e664044a8f210338c8894a95b35728f4800c92
ivansnow02 <ivansnow02@gmail.com>
2022-09-26 22:00:45 +0800

    大修改！

--- Action.java
+++ Action.java
@@ -1,8 +1,24 @@
+import java.util.Date;
+
+
 public class Action {
-    public static void main(String[] args) {
-        Gate gate = new Gate();
+    public static void main(String[] args) throws InterruptedException {
         NationalDay nationalday = new NationalDay();
-        gate.gatestate(nationalday.islockdown);
-        System.out.println(nationalday.name + nationalday.lockdown());
+        Date date = new Date();
+        int month = Integer.parseInt(String.format("%tm", date));
+        int day = Integer.parseInt(String.format("%td", date));
+        System.out.println("请稍等");
+        Thread.sleep(1000);
+        for (int i = 0; i < 10; i++) {
+            System.out.printf("*");
+            Thread.sleep(500);
+        }
+        System.out.println("");
+        if (month == 10 && day == 1) {
+            nationalday.HappyNationalDay();
+        }
+        else
+            System.out.println(nationalday.DaysToNationalDay(month, day));
+            
     }
 }
--- NationalDay.java
+++ NationalDay.java
@@ -1,11 +1,20 @@
 public class NationalDay {
-    boolean islockdown = true;
-    String name = "国庆";
+    final static String name = "国庆";
+    final static int month = 10;
+    final static int day = 1;
 
-    public String lockdown(){
-        if (this.islockdown)
-            return "你被封在学校里了";
-        else
-            return "你可以回家团聚了";
+    public String DaysToNationalDay(int Month, int Day) {
+        int MonthSub = month - Month;
+        int DaySub = day - Day;
+        int sub = MonthSub * 30 + DaySub;
+        if (sub < 0) {
+            return "距离国庆已经过了" + Integer.toString((-sub)) + "天了哦";
+        } else
+            return "还有" + Integer.toString(sub) + "天才到国庆";
+
+    }
+
+    public void HappyNationalDay() {
+        System.out.println("国庆快乐！");
     }
 }
