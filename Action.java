56a34687fb0cfe5d56b4ff51ecb5d62970ae4906
ivansnow02 <ivansnow02@gmail.com>
2022-09-26 20:25:53 +0800

    delete some files

--- Action.java
+++ Action.java
@@ -4,9 +4,5 @@ public class Action {
         NationalDay nationalday = new NationalDay();
         gate.gatestate(nationalday.islockdown);
         System.out.println(nationalday.name + nationalday.lockdown());
-        String epidemic;
-        epidemic = UrlUtils.sendGet("https://lab.isaaclin.cn/nCoV/api/area", "province=江苏省");
-        System.out.println(epidemic);
     }
 }
-// https://lab.isaaclin.cn/nCoV/api/area?province=%E6%B1%9F%E8%8B%8F%E7%9C%81
